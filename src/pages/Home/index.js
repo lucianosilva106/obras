0e1d132f30e0111cd1a73314f1e10aa48a361dde
Luciano Ferreira <l.ferreira1975@hotmail.com>
2022-08-09 14:45:34 -0300

    terceiro push

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,8 @@ import './home.css';
 function Home(){
     return(
         <div>
-            <h2>Você que deseja iniciar uma obra do zero, uma reforma completa, parcial ou ampliação de seu imóvel...</h2>
+            <h2>Você que deseja iniciar uma obra do zero, uma reforma completa, parcial ou ampliação de seu imóvel</h2> <p/>
+            <h2>antes de mais nada precisa contratar uma empresa especializada no assunto e que vai lhe garantir</h2>
         </div>
     );
 }
