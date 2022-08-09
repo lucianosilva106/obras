ebe0f91518e47cfff08853b7fbe4c529cc1626bd
Luciano Ferreira <l.ferreira1975@hotmail.com>
2022-08-09 10:14:34 -0300

    primeiro push

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,10 @@
+import './home.css';
+
 function Home(){
     return(
-        <div>Olá Mundo!</div>
+        <div>
+            <h2>Você que deseja iniciar uma obra do zero, uma reforma completa, parcial ou ampliação de seu imóvel...</h2>
+        </div>
     );
 }
 
