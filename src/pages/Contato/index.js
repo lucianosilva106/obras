b5a2e7bbc45aca0977969ccbb541090f39abe027
Luciano Ferreira <l.ferreira1975@hotmail.com>
2022-08-10 08:54:30 -0300

    adicionando dados de contato

--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -1,6 +1,9 @@
 function Contato(){
     return(
-        <div>Contato XX - XX XXXX XXXX</div>
+        <div>        
+            <h2>Olá, meu nome é Benedito Caetano e vou te acompanhar durante toda a solução do seu problema.</h2> <p/>
+            <h3>Meu e-mail é bene.caetano@uol.com.br e meu celular e whatsapp é ddd 11 99123 4567</h3>
+        </div>
     );
 }
 
